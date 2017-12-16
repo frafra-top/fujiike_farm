73550772da0dcb0984c09dbe9b735e83d4b5bbc6
Yomichi <Yomichi@Yomichi-no-MacBook-Pro.local>
2017-12-16 13:04:54 +0900

    テストデータ修正

--- db/fixtures/production/11_images.rb
+++ db/fixtures/production/11_images.rb
@@ -3,24 +3,24 @@ Image.seed(:id,
 {
   id: 1,
   item_id: 1,
-  file: Rails.root.join("db/fixtures/images/milk-750.jpg").open
+  file: Rails.root.join("db/fixtures/images/牛乳_720ml_1_定番のシンプルな味わいです.jpg").open
   },
 
 {
   id: 2,
   item_id: 2,
-  file: Rails.root.join("db/fixtures/images/milk-200.jpg").open
+  file: Rails.root.join("db/fixtures/images/牛乳_共通_2_毎朝の食卓にいかがでしょうか.jpg").open
   },
 
 {
   id: 3,
   item_id: 3,
-  file: Rails.root.join("db/fixtures/images/yoghurt.jpg").open
+  file: Rails.root.join("db/fixtures/images/ヨーグルト_1_シンプルながらも深い味わいです.jpg").open
   },
 
 {
   id: 4,
   item_id: 4,
-  file: Rails.root.join("db/fixtures/images/butter.jpg").open
+  file: Rails.root.join("db/fixtures/images/バター_1_特製パッケージに入れて配送しています.jpg").open
   }
 )
