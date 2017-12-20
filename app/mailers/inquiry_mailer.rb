5b2ac2c8640ada02bd16803c183a0176cafb3fef
Yomichi <Yomichi@Yomichi-no-MacBook-Pro.local>
2017-12-20 12:59:39 +0900

    文法エラー修正

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -1,10 +1,10 @@
 class InquiryMailer < ActionMailer::Base
- 
+
   def received_email(inquiry)
     @inquiry = inquiry
-    mail to: "ふじいけファームのメールアドレス"      
+    mail to: "ふじいけファームのメールアドレス",
     	 reply_to: @inquiry.email,
-         subject:  'お問い合わせを承りました'
+         subject: 'お問い合わせを承りました'
   end
- 
+
 end
